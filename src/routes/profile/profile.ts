e2f869ccc2f10ad4e636ff97b543ff68919b9cb6
daepang <dev.daepang@gmail.com>
2023-01-15 02:50:32 +0900

    profileDetail router add

--- src/routes/profile/profile.ts
+++ src/routes/profile/profile.ts
@@ -1,7 +1,19 @@
 import express, {Request, Response} from 'express'
+
+import profileDatailJson from '../../raw/profile/profileDetail.json'
 import FriendListJson from '../../raw/profile/friendList.json'
 
 const profileRouter = express.Router();
+
+const profileDetailData = {
+    code: "SUC_PROC_0000",
+    message: "요청이 정상적으로 처리되었습니다.",
+    responseTime: "20230112111359",
+    response: {
+        result: profileDatailJson,
+        status: "success"
+    }
+}
 
 const friendListData = {
     code: "SUC_PROC_0000",
@@ -12,6 +24,10 @@ const friendListData = {
         status: "success"
     }
 }
+
+profileRouter.get('/detail', async (req: Request, res: Response): Promise<any> => {
+    res.json(profileDetailData);
+})
 
 profileRouter.get('/friendList', async (req: Request, res: Response): Promise<any> => {
     res.json(friendListData);
