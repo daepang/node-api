c394363715f7af1c2492d8d26c8dfb9eb1fd6353
daepang <dev.daepang@gmail.com>
2023-04-04 14:43:24 +0900

    login.ts 수정

--- src/routes/login/login.ts
+++ src/routes/login/login.ts
@@ -1,16 +1,18 @@
 import express, {Request, Response} from 'express'
 
+import DateUtils from "../../utils/DateUtils";
+
 const loginRouter = express.Router();
 
 const loginResultData = {
     code: "SUC_PROC_0000",
     message: "요청이 정상적으로 처리되었습니다.",
-    responseTime: "20230112111359",
+    responseTime: DateUtils.getDateByTime('yyyyMMddhhmmss', '', ''),
     response: {
         status: "success"
     }
 }
-loginRouter.get('/login', async (req: Request, res: Response): Promise<any> => {
+loginRouter.get('/info', async (req: Request, res: Response): Promise<any> => {
     console.log("login req.params: ", req.params);
     console.log("login req.query: ", req.query);
     console.log("login req.body: ", req.body);
