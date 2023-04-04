ebff02cd08061c2b1251444012a83c92f346b22c
daepang <dev.daepang@gmail.com>
2023-04-04 16:04:02 +0900

    DateUtils.ts 수정

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,52 +1,35 @@
 const DateUtils = {
-    getDateByTime: function (formatType: string, timeData: string, dateGbn: string) {
+    getDateByTime: function (formatType: string, timeData: string) {
         let u = new Date();
-        if (timeData !== null && timeData !== "") {
-            if (typeof timeData == "string") {
-                u = this.getDateTime(timeData);
-            } else {
-                u = new Date(timeData);
-            }
+        if (timeData !== null && timeData !== '') {
+            u = this.getDateTime(timeData);
         }
-        const yyyy = u.getFullYear();
+        const yyyy = u.getFullYear().toString();
         const yy = u.getFullYear().toString().slice(-2);
-        let MM = ('0' + (u.getMonth() + 1)).slice(-2);
-        let dd = ('0' + u.getDate()).slice(-2);
+        const MM = ('0' + (u.getMonth() + 1)).slice(-2);
+        const dd = ('0' + u.getDate()).slice(-2);
         const hh = ('0' + u.getHours()).slice(-2);
         const mm = ('0' + u.getMinutes()).slice(-2);
         const ss = ('0' + u.getSeconds()).slice(-2);
-        let val;
-        if (dateGbn === null || dateGbn === "") {
-            dateGbn = "-";
-        }
-        switch (formatType) {
-            case "yyyy-MM-dd hh:mm:ss":
-                val = yyyy + dateGbn + MM + dateGbn + dd + " " + hh + ":" + mm + ":" + ss;
-                break;
-            case "yy-MM-dd hh:mm:ss":
-                val = yy + dateGbn + MM + dateGbn + dd + " " + hh + ":" + mm + ":" + ss;
-                break;
-            case "yyyy-MM-dd":
-                val = yyyy + dateGbn + MM + dateGbn + dd;
-                break;
-            case "yy-MM-dd":
-                val = yy + dateGbn + MM + dateGbn + dd;
-                break;
-            case "MM/dd":
-                val = MM + "/" + dd;
-                break;
-            case "MM월 dd일":
-                MM = MM.substring(0, 1) == '0' ? MM.substring(1,2) : MM;
-                dd = dd.substring(0, 1) == '0' ? dd.substring(1,2) : dd;
-                val = MM + "월 " + dd + "일";
-                break;
-            case "yyyyMMddhhmmss":
-                val = yyyy + '' + MM + '' + dd + '' + hh + '' + mm + '' + ss;
-                break;
-            default:
-                val = yyyy + "-" + MM + "-" + dd;
-        }
-        return val;
+        let returnVal: string = formatType;
+        returnVal = returnVal.replace('yyyy', yyyy);
+        returnVal = returnVal.replace('yy', yy);
+        returnVal = returnVal.replace('MM', MM);
+        returnVal = returnVal.replace('dd', dd);
+        returnVal = returnVal.replace('hh', hh);
+        returnVal = returnVal.replace('mm', mm);
+        returnVal = returnVal.replace('ss', ss);
+        return returnVal;
+    },
+    getDateTime: function (timeData: string) {
+        const year = parseInt(timeData.substring(0, 4));
+        const month = parseInt(timeData.substring(4, 6)) - 1;
+        const day = parseInt(timeData.substring(6, 8));
+        const hour = parseInt(timeData.substring(8, 10));
+        const minute = parseInt(timeData.substring(10, 12));
+        const second = parseInt(timeData.substring(12, 14));
+        const returnDate = new Date(year, month, day, hour, minute, second);
+        return returnDate;
     }
 };
 
