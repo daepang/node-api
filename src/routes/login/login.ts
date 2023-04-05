1421a7f8f8476d6509ccfe0e1bb62c5b2c0983c9
daepang <dev.daepang@gmail.com>
2023-04-05 14:51:02 +0900

    login.ts 수정

--- src/routes/login/login.ts
+++ src/routes/login/login.ts
@@ -9,10 +9,25 @@ const loginResultData = {
     message: "요청이 정상적으로 처리되었습니다.",
     responseTime: DateUtils.getDateByTime('yyyyMMddhhmmss', ''),
     response: {
-        status: "success"
+        "osType": "M",
+        "profileName": "DAEPANG",
+        "token": "token",
+        "loginId": "testAdmin"
     }
 }
 loginRouter.get('/info', async (req: Request, res: Response): Promise<any> => {
+    console.log("login req.params: ", req.params);
+    console.log("login req.query: ", req.query);
+    console.log("login req.body: ", req.body);
+
+    if ( req.query.id === 'testAdmin') {
+        res.json(loginResultData);
+    } else {
+        res.json({code: 'fail'});
+    }
+})
+
+loginRouter.post('/proc', async (req: Request, res: Response): Promise<any> => {
     console.log("login req.params: ", req.params);
     console.log("login req.query: ", req.query);
     console.log("login req.body: ", req.body);
