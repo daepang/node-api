925450335b24602b9ef30cb3b39846e41df3273e
daepang <dev.daepang@gmail.com>
2023-04-11 15:58:21 +0900

    login.ts 수정

--- src/routes/login/login.ts
+++ src/routes/login/login.ts
@@ -1,55 +1,48 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express';
 
-import DateUtils from "../../utils/DateUtils";
+import DateUtils from '../../utils/DateUtils';
 
 const loginRouter = express.Router();
 
 const loginResultData = {
-    code: "SUC_PROC_0000",
-    message: "요청이 정상적으로 처리되었습니다.",
-    responseTime: DateUtils.getDateByTime('yyyyMMddhhmmss', ''),
-    response: {
-        "osType": "M",
-        "profileName": "DAEPANG",
-        "token": "token",
-        "loginId": "testAdmin"
-    }
-}
+  code: 'SUC_PROC_0000',
+  message: '요청이 정상적으로 처리되었습니다.',
+  responseTime: DateUtils.getDateByTime('yyyyMMddhhmmss', ''),
+  response: {
+    osType: 'M',
+    profileName: 'DAEPANG',
+    token: 'token',
+    loginId: 'testAdmin',
+  },
+};
 
-const loginFailData = {
-    code: "FAIL_0000",
-    message: "ID를 입력해주세요.",
-    responseTime: DateUtils.getDateByTime('yyyyMMddhhmmss', '')
-}
+const idEmptyRes = {
+  code: 'FAIL_ID_EMPTY',
+  message: '아이디를 입력해주세요.',
+  responseTime: DateUtils.getDateByTime('yyyyMMddhhmmss', ''),
+};
+
+const passwordEmptyRes = {
+  code: 'FAIL_PASSWORD_EMPTY',
+  message: '비밀번호를 입력해주세요.',
+  responseTime: DateUtils.getDateByTime('yyyyMMddhhmmss', ''),
+};
 
 loginRouter.post('/proc', async (req: Request, res: Response): Promise<any> => {
-    console.log("login req.params: ", req.params);
-    console.log("login req.query: ", req.query);
-    console.log("login req.body: ", req.body);
+  console.log('login req.params: ', req.params);
+  console.log('login req.query: ', req.query);
+  console.log('login req.body: ', req.body);
 
-    const response = {...loginResultData.response, loginId: req.query.id};
-    const result = {...loginResultData, response: response};
+  const response = { ...loginResultData.response, loginId: req.query.id };
+  const result = { ...loginResultData, response: response };
 
-    if ( req.query.id ) {
-        res.json(result);
-    } else {
-        res.json(loginFailData);
-    }
-})
+  if (req.query.id && req.query.password) {
+    res.json(result);
+  } else if (!req.query.id) {
+    res.json(idEmptyRes);
+  } else if (!req.query.password) {
+    res.json(passwordEmptyRes);
+  }
+});
 
-loginRouter.post('/check', async (req: Request, res: Response): Promise<any> => {
-    console.log("login req.params: ", req.params);
-    console.log("login req.query: ", req.query);
-    console.log("login req.body: ", req.body);
-
-    const response = {...loginResultData.response, loginId: req.query.id};
-    const result = {...loginResultData, response: response};
-
-    if ( req.query.id ) {
-        res.json(result);
-    } else {
-        res.json(loginFailData);
-    }
-})
-
-export { loginRouter }
+export { loginRouter };
